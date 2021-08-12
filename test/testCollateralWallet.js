11be79adf5a704fff5a9983e5b99a31a1c339a03
Tamas An <tamasan77@gmail.com>
2021-08-12 04:41:16 -0500

    wallet tested

--- test/testCollateralWallet.js
+++ test/testCollateralWallet.js
@@ -1,13 +1,17 @@
+const { assert } = require("chai");
+
 const CollateralWallet = artifacts.require("./CollateralWallet.sol");
 const TestERC20Token = artifacts.require("./TestERC20Token.sol");
+const FFAContract = artifacts.require("./FFAContract.sol");
 
 contract("CollaterWallet", accounts => {
     it("should be able to construct CollateralWallet, set and get new balance", async function() {
-        const CollateralWalletInstance = await CollateralWallet.deployed();
-        const TestERC20TokenInstance = await TestERC20Token.deployed();
+        const collateralWalletInstance = await CollateralWallet.deployed();
+        const testERC20TokenInstance = await TestERC20Token.deployed();
+        const ffaContractInstance = await FFAContract.deployed();
 
-        
-        
-
+        await collateralWalletInstance.setNewBalance(ffaContractInstance.address, testERC20TokenInstance.address, 200);
+        const newBalance = await collateralWalletInstance.getMappedBalance(ffaContractInstance.address, testERC20TokenInstance.address);
+        assert.equal(newBalance, 200, "balances not equal");
     })
 });
