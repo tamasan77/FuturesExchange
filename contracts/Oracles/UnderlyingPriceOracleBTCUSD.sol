8600d810954ed18100a6cb715e185607536c6ed0
Tamas An <tamasan77@gmail.com>
2021-08-20 06:40:00 -0500

    BTC-USD UnderlyingPriceOrcale.sol done

---
+++ contracts/Oracles/UnderlyingPriceOracleBTCUSD.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.6;
+
+import "../ChainlinkOracle.sol";
+
+contract UnderlyingPriceOracleBTCUSD is ChainlinkOracle {
+    string private constant apiURL = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR";
+    string public constant apiPath = "USD";
+    int8 decimals = 10 ** 2;
+    
+
+    constructor(
+        address oracleAddress,
+        bytes32 jobId,
+        address linkAddress,
+        uint256 fee) 
+        ChainlinkOracle (
+            oracleAddress,
+            jobId,
+            apiURL, 
+            apiPath,
+            linkAddress,
+            fee,
+            decimals
+        ) {
+    }
+}
