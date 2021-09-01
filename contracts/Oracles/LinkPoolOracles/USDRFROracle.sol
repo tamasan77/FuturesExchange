bd0de6601bd25524caf4f2dfc390a0a04710f0b4
Tamas An <tamasan77@gmail.com>
2021-09-01 08:43:25 -0500

    test json path for nested arrays

--- contracts/Oracles/LinkPoolOracles/USDRFROracle.sol
+++ contracts/Oracles/LinkPoolOracles/USDRFROracle.sol
@@ -46,11 +46,42 @@ contract USDRFROracle is LinkPoolIntOracle {
     //start date will be a api URL parameter
     string public constant _apiBaseURL_ = "https://www.quandl.com/api/v3/datasets/USTREASURY/BILLRATES.json?api_key=";
     //use 0 index for data array since we always want the most recent value
-    string public constant _apiPath_ = "dataset.data[0]";
+    string public constant _apiPath_ = "dataset.data[0][";
+
     constructor(
        string memory startDate,
        uint8 maturityTranchIndex
     ) LinkPoolIntOracle(
         _decimals_
-    )
+    ) {}
+    
+     //concatanate strings and add /  where needed for URL
+    function constructApiPath(string memory a, string memory b) internal pure returns (string memory) {
+	    return string(abi.encodePacked(a,"/", b, "/", c, "/", d));
+    }
+
+    
+    //parse uint to string
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
 }
