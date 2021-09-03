40010bc886322eb7afb2cd29d3bdbe8ec66636b5
Tamas An <tamasan77@gmail.com>
2021-09-03 09:55:07 -0500

    create forward frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import FFAContract from "./contracts/FFAContract.json";
-import FFAFactory from "./constracts/FFAFactory.json"
+import FFAFactory from "./contracts/FFAFactory.json"
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = {loaded:false, contractName: "Example Forward Contract", contractSymbol: "EFC"};
+  state = {loaded:false, /*contractName: "Example Forward Contract", contractSymbol: "EFC"*/};
 
   componentDidMount = async () => {
     try {
@@ -18,14 +18,15 @@ class App extends Component {
 
       // Get the contract instance.
       this.networkId = await this.web3.eth.net.getId();
-      this.ffaContract = new web3.eth.Contract(
+
+      this.ffaContract = new this.web3.eth.Contract(
         FFAContract.abi,
-        FFAContract.networks[networkId] && FFAContract.networks[networkId].address,
+        FFAContract.networks[this.networkId] && FFAContract.networks[this.networkId].address,
       );
 
-      this.ffaFactory = new web3.eth.Contract(
+      this.ffaFactory = new this.web3.eth.Contract(
         FFAContract.abi,
-        FFAFactory.networks[networkId] && FFAFactory.networks[networkId].address,
+        FFAFactory.networks[this.networkId] && FFAFactory.networks[this.networkId].address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -40,6 +41,20 @@ class App extends Component {
     }
   };
 
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = async() => {
+    const {contractName, contractSymbol, contractSize, underlyingAPIURL, underlyingAPIPath, underlyingDecimals} = this.state;
+    await this.ffaFactory.methods.createFFAContract(contractName, contractSymbol, contractSize ,underlyingAPIURL, underlyingAPIPath, underlyingDecimals).send({from: this.accounts[0]});
+  }
+
   render() {
     if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -48,8 +63,12 @@ class App extends Component {
       <div className="App">
         <h1>Forward Contract Factory</h1>
         <h2>Create Forward Contract</h2>
-        Contract Name: <input type="text" name="contractName" value={this.state.contractName} onChange={} />
-        Contract Symbol: <input type="text" name="contractSymbol" value={this.state.contractSymbol} onChange={} />
+        Contract Name: <input type="text" name="contractName" value={this.state.contractName} onChange={this.handleInputChange} />
+        Contract Symbol: <input type="text" name="contractSymbol" value={this.state.contractSymbol} onChange={this.handleInputChange} />
+        Contract Size: <input type="number" name="contractSize" value={this.state.contractSize} onChange={this.handleInputChange} />
+        Underlying API URL: <input type="text" name="underlyingAPIURL" value={this.state.underlyingAPIURL} onChange={this.handleInputChange} />
+        Underlying API Path: <input type="text" name="underlyinAPIPath" value={this.state.underlyinAPIPath} onChange={this.handleInputChange} />
+        UnderlyingDecimals: <input type="number" name="underlyingDecimals" value={this.state.underlyingDecimals} onChange={this.handleInputChange} />
         <button type="button" onClick={this.handleSubmit}>Create new forward contract</button>
       </div>
     );
